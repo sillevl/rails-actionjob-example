355d581e4e249307adc0e5d5ed5ad91f4e265f97
Sille Van Landschoot <info@sillevl.be>
2016-11-20 10:16:46 +0100

    add work to the new job

--- app/jobs/awesome_job.rb
+++ app/jobs/awesome_job.rb
@@ -2,6 +2,9 @@ class AwesomeJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    # Do something later
+      loop do
+          puts "Hello from my awesome job"
+          sleep 5
+      end
   end
 end
